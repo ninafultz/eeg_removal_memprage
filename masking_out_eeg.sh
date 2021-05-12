415bcb8a04933c1723a6330ae2148571ba0873ec
ninafultz <46796811+ninafultz@users.noreply.github.com>
2021-05-12 18:03:47 -0400

    Update masking_out_eeg.sh

--- masking_out_eeg.sh
+++ masking_out_eeg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # how to write it out: ./masking_out_eeg.sh -n racsleep08
-# nina fultz may 2021
+# nina fultz may 2021: contact nfultz@mgh.harvard.edu if you run into any problems
 
 #gets displayed when -h or --help is put in
 display_usage(){
     echo "***************************************************************************************
 Script to take bias corrected, eroded, skull mask and apply it to MEMPRAGE to remove eeg artifact 
 *************************************************************************************** "
-    echo Usage: ./masking_out_eeg.sh -n -d -b
+    echo Usage: ./masking_out_eeg.sh -n -d
        -n: Name of subject
 }
 
@@ -30,14 +30,14 @@ export EEG_REMOVAL=$DIR/$SUBJECT/eeg_removal
 module load fsl
 module load freesurfer
 
-
 #manually threshold and check bone
-#save out thresholded file as: $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_bone_thresholded.nii.gz.  - threshold bone! go to tools>threshold>the gui #low: 0.06, high:70, press run, then look at bone image
+#save out thresholded file as: $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_bone_thresholded.nii.gz.  
+        #- threshold bone! go to tools>threshold>the gui 
+        #low: 0.06, high:70, press run, then look at bone image
 #save out thresholded file as: $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_other_thresholded.nii.gz - erode them: tools>volume filter> erode
 #adding all of WM, GM, CSF, bone, other together
 
 fslmaths $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_CSF.nii.gz -add $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_GM.nii.gz -add $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_WM.nii.gz -add $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_bone_thresholded.nii.gz -add $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_other_thresholded.nii.gz $DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_other_WM_CSF_GM_bone.nii.gz
-
 
 FILE=$DIR/$SUBJECT/eeg_removal/MEMPR_iso1mm_other_WM_CSF_GM_bone.nii.gz   
 if [ -f $FILE ]; then
@@ -45,7 +45,6 @@ if [ -f $FILE ]; then
 else
    echo "File ${FILE##*/} does not exist. Something went wrong with adding the CSF, GM, bone, other, and WM together! "
 fi
-
 
 #masking out MEMPRAGE with this MEMPR_iso1mm_other_eroded_and_WM_CSF_GM_bone.nii.gz file: 
     #-mas   : use (following image>0) to mask current image
